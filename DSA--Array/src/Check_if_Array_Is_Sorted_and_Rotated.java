9174a4c5fb1ba06b4cc6486dcb9a1478fb8c4f25
Shilpa raveendran <113525124+shilpa-raveendran@users.noreply.github.com>
2024-09-26 00:36:39 -0400

    Update Check_if_Array_Is_Sorted_and_Rotated.java

--- DSA--Array/src/Check_if_Array_Is_Sorted_and_Rotated.java
+++ DSA--Array/src/Check_if_Array_Is_Sorted_and_Rotated.java
@@ -4,27 +4,45 @@ public class Check_if_Array_Is_Sorted_and_Rotated {
        int nums[]={1,2,6,3,4,5};
        System.out.println(isSortedOptimal(nums));
    }
-
-   //brute-force
+   //Aproach
+   //A sorted array that is rotated would have at most one "drop" or point where an element is greater than the next element.
+   //Check Wrap Around: After the loop, check if the first element is less than the last element. If it is, increment count since this implies a "drop" from the end of the array to the start.
+   //we need to check if there is at most one such "drop" point.
+   
+   //brute-force----> T.C= O(n^2)
     private static boolean isSorted_Bf(int[] nums) {
+       int count=0;
         for (int i = 0; i <= nums.length-1; i++) {
             for (int j = i+1; j < nums.length; j++) {
                 if(nums[i] > nums[j]){
-                    return false;
+                   count++;
                 }
             }
 
         }
-       return true;
+       if(nums[0] < nums[nums.length-1]){
+            count++;
+        }
+        if(count <=1){
+            return true;
+        }
+        return false;
     }
 
-    //optimal-method
+    //optimal-method----> T.C= O(n)
     private static boolean isSortedOptimal(int[] nums) {
+       int count=0;
         for (int i = 1; i <= nums.length-1; i++) {
                 if(nums[i-1] > nums[i]){
-                    return false;
+                    count++;
                 }
         }
-        return true;
+       if(nums[0] < nums[nums.length-1]){
+            count++;
+        }
+        if(count <=1){
+            return true;
+        }
+        return false;
     }
 }
