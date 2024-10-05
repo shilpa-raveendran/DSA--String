884c20ecf0d414d99ea99792849024e5bd03528e
Shilpa raveendran <113525124+shilpa-raveendran@users.noreply.github.com>
2024-10-05 00:12:29 -0400

    Create MoveZeroes.java

---
+++ DSA--Array/src/MoveZeroes.java
@@ -0,0 +1,45 @@
+public class MoveZeroes {
+// LC: 283. Move Zeroes
+    public static void main(String[] args) {
+        int nums[] = {0, 1, 0, 3, 1, 12};
+        //bruteForce(nums);
+        optimalSol(nums);
+        for (int i = 0; i < nums.length; i++) {
+            System.out.print(nums[i] + " ");
+        }
+    }
+
+    private static void bruteForce(int[] nums) {
+        int temp[] = new int[nums.length];
+        int j = 0;
+        for (int i = 0; i < nums.length; i++) {
+
+            if (nums[i] != 0) {
+                temp[j] = nums[i];
+                j++;
+            }
+
+        }
+        for (int i = 0; i < nums.length; i++) {
+            nums[i] = temp[i];
+            System.out.print(nums[i] + " ");
+        }
+    }
+
+    private static void optimalSol(int[] nums) {
+        int j = 0;  // Pointer to track the position of the next non-zero element
+
+        // Move all non-zero elements to the front of the array
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != 0) {
+                nums[j] = nums[i];
+                j++;
+            }
+        }
+
+        // Fill the remaining positions with zeros
+        for (int i = j; i < nums.length; i++) {
+            nums[i] = 0;
+        }
+    }
+}
