9057a2d390ca948cdc47e44b5eb9441861e82325
Shilpa Raveendran <silpa304@gmail.com>
2024-10-28 00:14:11 -0400

    Intersection of two sorted arrays-Better solution

--- DSA--Array/src/IntersectionOfArrays.java
+++ DSA--Array/src/IntersectionOfArrays.java
@@ -2,14 +2,14 @@ import java.util.ArrayList;
 
 public class  IntersectionOfArrays {
     public static void main(String[] args) {
-        int[] a={1,2,2,3,4};
-        int[] b={2,2,3};
+        int[] a={2,2,3,4};
+        int[] b={1,2,2,3};
         findIntersection(a,b).forEach(s-> System.out.print(s+" "));
     }
     public static ArrayList<Integer> findIntersection(int a[], int b[]){
         ArrayList<Integer> result= new ArrayList<>();
         //brute force
-        int visted[]=new int[b.length];
+        /*int visted[]=new int[b.length];
         for (int i = 0; i < a.length; i++) {
             for (int j = 0; j < b.length; j++) {
                 if(a[i]==b[j] && visted[j]==0){
@@ -19,6 +19,23 @@ public class  IntersectionOfArrays {
                 }else if(b[j] >a[i]){
                     break;
                 }
+            }
+        }*/
+
+        //Better solution-- Two pointers
+        int i = 0; int j= 0;
+        int n = a.length;
+        int m = b.length;
+
+        while(i < n && j <m){
+            if(a[i] == b[j]){
+                result.add(a[i]);
+                i++;
+                j++;
+            }else if(a[i] < b[j]){
+                i++;
+            }else{
+                j++;
             }
         }
         return result;
