ceeb48c1b097701e5cf46f2e8c080234e5a6a704
Shilpa Raveendran <silpa304@gmail.com>
2024-11-01 00:47:26 -0400

    LC: 560. Subarray Sum Equals K

--- DSA--Array/src/SubArraySum.java
+++ DSA--Array/src/SubArraySum.java
@@ -1,15 +1,39 @@
 public class SubArraySum {
     public static void main(String[] args) {
-        int[] nums ={1,2,1,2,1};
-        int k =3;/// lengthOfLongestSubArray = 2; //totalNoOfSubarrays=4
+        int[] nums ={1,1,1};
+        int k =2;/// lengthOfLongestSubArray = 2; //totalNoOfSubarrays=2
         int lengthOfLongestSubArray= findLengthOfLongSubarray( nums,  k);
         System.out.println("The Length Of Longest Subarray:"+lengthOfLongestSubArray);
+
+        int totalNoOfSubarrays= findTotalNoOfSubarrays( nums,  k);
+        System.out.println("The Total No of Subarrays with K sum:"+totalNoOfSubarrays);
     }
 
+    //LC: 560. Subarray Sum Equals K
     public static int findTotalNoOfSubarrays(int[] nums, int k) {
         //brute force
         int res=0;
-        //int count=0;
+        int count=0;
+        for(int i = 0 ; i < nums.length ; i++){
+
+            int sum=nums[i];
+            if (sum == k) {
+                count++;
+            }
+            for (int j = i+1 ; j < nums.length ; j++) {
+                sum = sum + nums[j];
+                if (sum == k) {
+                    count++;
+                }
+            }
+
+        }
+        return count;
+    }
+
+
+    public static int findLengthOfLongSubarray(int[] nums, int k) {
+        int res=0;
         for(int i = 0 ; i < nums.length ; i++){
             int count=0;
             int sum=nums[i];
@@ -24,10 +48,6 @@ public class SubArraySum {
             res= Math.max(count,res);
         }
         return res;
-    }
 
-    public static int findLengthOfLongSubarray(int[] nums, int k) {
-
-        return 0;
     }
 }
