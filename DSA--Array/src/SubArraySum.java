d270c4f19b9442cb0726c5bd7a3eb439fd8bec9e
Shilpa Raveendran <silpa304@gmail.com>
2024-11-03 21:08:37 -0500

    LC: 560. Subarray Sum Equals K-Optimal solution(Hashing)

--- DSA--Array/src/SubArraySum.java
+++ DSA--Array/src/SubArraySum.java
@@ -1,21 +1,24 @@
+import java.util.HashMap;
+
 public class SubArraySum {
     public static void main(String[] args) {
-        int[] nums ={1,1,1};
-        int k =2;/// lengthOfLongestSubArray = 2; //totalNoOfSubarrays=2
-        int lengthOfLongestSubArray= findLengthOfLongSubarray( nums,  k);
+        int[] nums ={0,0,0,0,0,0,0,0,0,0};
+        int k =0;/// lengthOfLongestSubArray = 10; //totalNoOfSubarrays=55
+        int lengthOfLongestSubArray= findLengthOfLongSubArrayHashing( nums,  k);
         System.out.println("The Length Of Longest Subarray:"+lengthOfLongestSubArray);
 
-        int totalNoOfSubarrays= findTotalNoOfSubarrays( nums,  k);
+        int totalNoOfSubarrays= findTotalNoOfSubArrayHashing( nums,  k);
         System.out.println("The Total No of Subarrays with K sum:"+totalNoOfSubarrays);
     }
 
     //LC: 560. Subarray Sum Equals K
-    public static int findTotalNoOfSubarrays(int[] nums, int k) {
+    public static int findTotalNoOfSubArray(int[] nums, int k) {
         //brute force
         int count=0;
         for(int i = 0 ; i < nums.length ; i++){
+
             int sum=0;
-            for (int j = i+1 ; j < nums.length ; j++) {
+            for (int j = i ; j < nums.length ; j++) {
                 sum = sum + nums[j];
                 if (sum == k) {
                     count++;
@@ -25,8 +28,27 @@ public class SubArraySum {
         return count;
     }
 
+    public static int findTotalNoOfSubArrayHashing(int[] nums, int k){
+        HashMap<Integer, Integer> map= new HashMap<Integer,Integer>();
+        int count=0;
+        int prefixSum=0;
+        for(int i = 0 ; i < nums.length; i++){
+            prefixSum += nums[i];
 
-    public static int findLengthOfLongSubarray(int[] nums, int k) {
+            if(prefixSum == k){
+                count++;
+            }
+
+            int remainingSum= prefixSum-k;
+            if(map.containsKey(remainingSum)){
+                count = count+ map.get(remainingSum);
+            }
+            map.put(prefixSum, map.getOrDefault(prefixSum, 0) + 1);
+        }
+        return count;
+    }
+    //Brtue Force
+    public static int findLengthOfLongSubArray(int[] nums, int k) {
         int res=0;
         for(int i = 0 ; i < nums.length ; i++){
             int count=0;
@@ -41,5 +63,26 @@ public class SubArraySum {
         }
         return res;
 
+    }
+    public static int findLengthOfLongSubArrayHashing(int[] nums, int k) {
+        //Declare a map to store the prefix sums and the indices.
+        HashMap<Integer,Integer> map= new HashMap<>();
+        int sum=0;
+        int maxLength=0;
+        for (int i = 0; i < nums.length; i++) {
+            sum+=nums[i];
+            if(sum ==k){
+                maxLength=Math.max(maxLength,i+1);
+            }
+            int remainingSum= sum-k;
+            if (map.containsKey(remainingSum)){
+                int length= i-map.get(remainingSum);
+                maxLength=Math.max(maxLength,length);
+            }
+            if (!map.containsKey(remainingSum)){
+                map.put(sum,i);
+            }
+        }
+        return maxLength;
     }
 }
