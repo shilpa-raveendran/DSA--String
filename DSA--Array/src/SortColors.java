0f3cd7b8151d1a18822d3616851fff85e5f0b503
Shilpa Raveendran <silpa304@gmail.com>
2024-11-07 08:08:02 -0500

    LC-75 Sort Colors-Optimal Solution( Dutch Flag Algo)

--- DSA--Array/src/SortColors.java
+++ DSA--Array/src/SortColors.java
@@ -2,18 +2,40 @@ public class SortColors {
     public static void main(String[] args) {
         int[] nums ={2,0,2,1,1,0};
         //Brute Force--> Arrays.sort(nums)->O(nlogn)
-        sortColorsBetterSol(nums);
+        sortColors(nums);
         for (int num : nums) {
             System.out.print(num + " ");
         }
     }
-
+    //Opimal Sol- Dutch Flag Algo
+    //Single pass-O(n)
     private static int[] sortColors(int[] nums) {
+        int low=0;
+        int mid=0;
+        int high=nums.length-1;
+        while(mid<=high){
+            if(nums[mid]==0){
+                int temp=nums[low];
+                nums[low]=nums[mid];
+                nums[mid]=temp;
+                low++;
+                mid++;
+            }else if(nums[mid]==1){
+                mid++;
+            }else{
+                int temp=nums[high];
+                nums[high]=nums[mid];
+                nums[mid]=temp;
+                high--;
+            }
+        }
         return nums;
     }
 
+
     //Better sol;
     //Keep a count variable for 0s,1,s and 2s
+    //TC=O(n)
     private static int[] sortColorsBetterSol(int[] nums) {
         int count1=0;
         int count2=0;
