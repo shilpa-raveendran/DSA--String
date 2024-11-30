b9a5a89a84c6f98dcf39d5d23c5b144efeb8d55b
Shilpa Raveendran <silpa304@gmail.com>
2024-11-29 22:46:46 -0500

    LC 121-best-time-to-buy-and-sell-stock

--- DSA--Array/src/BuyStockAndSell.java
+++ DSA--Array/src/BuyStockAndSell.java
@@ -16,4 +16,14 @@ public class BuyStockAndSell {
         }
         return max;
     }
+    //Optimal solution
+   public static int maxProfit(int[] prices) {
+        int maxPro = 0;
+        int minPrice = Integer.MAX_VALUE;
+        for (int i = 0; i < prices.length; i++) {
+            minPrice = Math.min(minPrice, prices[i]);
+            maxPro = Math.max(maxPro, prices[i] - minPrice);
+        }
+        return maxPro;
+    }
 }
